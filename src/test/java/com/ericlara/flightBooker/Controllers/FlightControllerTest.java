c1b0091207dc2f20283553a1dcb6c319e97335b0
rock-and-code <105887521+rock-and-code@users.noreply.github.com>
2023-06-10 17:06:47 -0400

    adding JUnit test for Flights web controller

--- src/test/java/com/ericlara/flightBooker/Controllers/FlightControllerTest.java
+++ src/test/java/com/ericlara/flightBooker/Controllers/FlightControllerTest.java
@@ -1,7 +1,14 @@
 package com.ericlara.flightBooker.Controllers;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.mockito.BDDMockito;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,81 +16,121 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
-import com.ericlara.flightBooker.Models.UserDto;
+import org.mockito.Mockito;
+import com.ericlara.flightBooker.Models.AirportData;
+import com.ericlara.flightBooker.Models.Flight;
+import com.ericlara.flightBooker.Models.FlightDto;
 import com.ericlara.flightBooker.Services.AirportService;
 import com.ericlara.flightBooker.Services.FlightService;
 import com.ericlara.flightBooker.Services.UserService;
+
 @WebMvcTest(FlightController.class)
-// This annotation tells Spring Boot to create a MockMvc instance for testing the FlightController class.
+// This annotation tells Spring Boot to create a MockMvc instance for testing
+// the FlightController class.
 
 @AutoConfigureMockMvc(addFilters = false) // TO CIRCUMVENT SPRING SECURITY
-// This annotation tells Spring Boot to not add any Spring Security filters to the MockMvc instance. This is necessary because we are testing the FlightController class without Spring Security enabled.
+// This annotation tells Spring Boot to not add any Spring Security filters to
+// the MockMvc instance. This is necessary because we are testing the
+// FlightController class without Spring Security enabled.
 
 @ExtendWith(MockitoExtension.class)
-// This annotation tells JUnit to use the Mockito framework for mocking dependencies.
+// This annotation tells JUnit to use the Mockito framework for mocking
+// dependencies.
 
 public class FlightControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    // This field is injected by Spring Boot. It provides a MockMvc instance that can be used to test the FlightController class.
-
-    @MockBean
-    private AuthenticationManager authenticationManager;
-    // This field is mocked by Mockito. It provides a mock implementation of the AuthenticationManager interface. This is necessary because we are testing the FlightController class without Spring Security enabled.
+    // This field is injected by Spring Boot. It provides a MockMvc instance that
+    // can be used to test the FlightController class.
 
     @MockBean(name = "flightService")
     private FlightService flightService;
-    // This field is mocked by Mockito. It provides a mock implementation of the FlightService interface. This is necessary because we want to test the FlightController class in isolation.
+    // This field is mocked by Mockito. It provides a mock implementation of the
+    // FlightService interface. This is necessary because we want to test the
+    // FlightController class in isolation.
 
     @MockBean(name = "airportService")
     private AirportService airportService;
-    // This field is mocked by Mockito. It provides a mock implementation of the AirportService interface. This is necessary because we want to test the FlightController class in isolation.
+    // This field is mocked by Mockito. It provides a mock implementation of the
+    // AirportService interface. This is necessary because we want to test the
+    // FlightController class in isolation.
 
     @MockBean(name = "userService")
     private UserService userService;
-    // This field is mocked by Mockito. It provides a mock implementation of the UserService interface. This is necessary because we want to test the FlightController class in isolation.
+    // This field is mocked by Mockito. It provides a mock implementation of the
+    // UserService interface. This is necessary because we want to test the
+    // FlightController class in isolation.
 
     @Test
     void testFlightsForm() throws Exception {
+
+        this.mockMvc.perform(get("/")).andExpect(view().name("flights/flightSearchForm"))
+                .andDo(MockMvcResultHandlers.print());
+        // This performs a GET request to the "/" URL. This request is expected to
+        // return the "flights/flightSearchForm" view. The MockMvcResultHandlers.print()
+        // method is used to print the results of the request.
+    }
+
+    @Test
+    void searchResultsTest() throws Exception {
         // This method tests the FlightController.flightsForm() method.
 
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        // This creates a new MockHttpServletRequest object. This request will be used to simulate a user request to the FlightController.flightsForm() method.
+        FlightDto flightDto = FlightDto.builder()
+        .departureDate(LocalDate.of(2022, 05, 30))
+        .destination(AirportData.usAirports.get(1).getFormattedLocation())
+        .origin(AirportData.usAirports.get(0).getFormattedLocation())
+        .numOfPassengers(3)
+        .build();
 
-        //CREATES A USER FOR DEMOSTRATION PURPOSES ONLY, THE APP ALLOWS NEW USERS TO BE REGISTERED
-        UserDto user = new UserDto("userFirstName", "userLastName", "user@email.com", "password");
-        // This creates a new UserDto object. This user will be used to simulate a user who is logged in to the application.
+        this.mockMvc.perform(post("/flights")
+                .flashAttr("flightDto", flightDto)
+                ).andExpect(view().name("flights/flights"))
+                .andDo(MockMvcResultHandlers.print());
+        // This performs a GET request to the "/" URL. This request is expected to
+        // return the "flights/flightSearchForm" view. The MockMvcResultHandlers.print()
+        // method is used to print the results of the request.
+    }
 
-        //SAVES USER TO THE MEMORY DBA
-        userService.register(user);
-        // This saves the user to the in-memory database. This is necessary because we want to simulate a user who is logged in to the application.
+    @Test
+    void getSearchResultsTest() throws Exception {
+       
+        this.mockMvc.perform(get("/flights")
+                .param("origin", "origin")
+                .param("destination", "destination")
+                .param("departureDate", "2023-05-30")
+                ).andExpect(view().name("flights/flights"))
+                .andDo(MockMvcResultHandlers.print());
+        // This performs a GET request to the "/" URL. This request is expected to
+        // return the "flights/flightSearchForm" view. The MockMvcResultHandlers.print()
+        // method is used to print the results of the request.
+    }
 
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
-        authToken.setDetails(new WebAuthenticationDetails(request));
-        // This creates a new UsernamePasswordAuthenticationToken object. This object will be used to simulate a user who is logged in to the application.
+    @Test
+    void testgetFlightDetails() throws Exception {
+        Long flightId = 1L;
+        Flight flight = Flight.builder()
+        .id(flightId)
+        .departureDate(LocalDate.of(2022, 05, 30))
+        .departureTime(LocalTime.of(12, 30, 0))
+        .origin(AirportData.usAirports.get(0).getFormattedLocation())
+        .destination(AirportData.usAirports.get(1).getFormattedLocation())
+        .availableSeats(50)
+        .flightNumber("MX6789")
+        .price(150.45)
+        .build();
+        BDDMockito.given(flightService.findFlightById(any(Long.class))).willReturn(flight);
+        Mockito.when(flightService.findFlightById(flightId)).thenReturn(flight);
 
-        Authentication authentication = authenticationManager.authenticate(authToken);
-        // This authenticates the user. This is necessary because we want to simulate a user who is logged in to the application.
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        // This sets the current security context to the authenticated user. This is necessary because we want to simulate a user who is logged in to the application.
-
-        this.mockMvc.perform(get("/")
-        ).andExpect(view().name("flights/flightSearchForm"))
-        .andDo(MockMvcResultHandlers.print());
-        // This performs a GET request to the "/" URL. This request is expected to return the "flights/flightSearchForm" view. The MockMvcResultHandlers.print() method is used to print the results of the request.
-
+        // this.mockMvc.
+        this.mockMvc.perform(get("/flights/{flightId}", 1L))
+                .andExpect(view().name("flights/flightDetails"))
+                .andDo(MockMvcResultHandlers.print());
+        // This performs a GET request to the "/" URL. This request is expected to
+        // return the "flights/flightSearchForm" view. The MockMvcResultHandlers.print()
+        // method is used to print the results of the request.
     }
 
 }
-
